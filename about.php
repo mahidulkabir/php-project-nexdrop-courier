9c917fff99ae49fc221bb2030ba46fdb6689c611
mahidulkabir <mahidulkabir8@gmail.com>
2025-08-10 12:42:29 +0600

    front end completed

--- about.php
+++ about.php
@@ -1,16 +1,18 @@
-<?php require_once('./includes/head.php')?>
+<?php require_once('./includes/head.php') ?>
 
 <body class="starter-page-page">
 
-<?php require_once('./includes/navbar.php')?>
+  <?php require_once('./includes/navbar.php') ?>
 
   <main class="main">
 
     <!-- Page Title -->
-    <div class="page-title dark-background" data-aos="fade" style="background-image: url(assets/img/page-title-bg.jpg);">
+    <div class="page-title dark-background" data-aos="fade"
+      style="background-image: url(assets/img/page-title-bg.jpg);">
       <div class="container position-relative">
         <h1>About Us</h1>
-        <p>At NEXDROP Courier, we are dedicated to delivering packages with speed, safety, and care, connecting people and businesses worldwide.</p>
+        <p>At NEXDROP Courier, we are dedicated to delivering packages with speed, safety, and care, connecting people
+          and businesses worldwide.</p>
         <nav class="breadcrumbs">
           <ol>
             <li><a href="index.html">Home</a></li>
@@ -20,44 +22,49 @@
       </div>
     </div><!-- End Page Title -->
 
-     <!-- About Section -->
+    <!-- About Section -->
     <section id="about" class="about section">
 
       <div class="container">
 
         <div class="row gy-4">
 
-          <div class="col-lg-6 position-relative order-lg-last order-first d-flex align-items-center" data-aos="fade-up" data-aos-delay="200">
+          <div class="col-lg-6 position-relative order-lg-last order-first d-flex align-items-center" data-aos="fade-up"
+            data-aos-delay="200">
             <img src="assets/img/about.jpg" class="img-fluid" alt="">
           </div>
 
           <div class="col-lg-6 content order-last  order-lg-first" data-aos="fade-up" data-aos-delay="100">
             <h3>About Us</h3>
             <p>
-             NEXDROP Courier is committed to making deliveries faster, safer, and more reliable.
-We combine modern technology with a customer-first approach to ensure every package reaches its destination on time.
-With worldwide reach, we connect people and businesses, one delivery at a time.
+              NEXDROP Courier is committed to making deliveries faster, safer, and more reliable.
+              We combine modern technology with a customer-first approach to ensure every package reaches its
+              destination on time.
+              With worldwide reach, we connect people and businesses, one delivery at a time.
             </p>
             <ul>
               <li>
                 <i class="bi bi-people"></i>
                 <div>
                   <h5> Professional & Trained Staff</h5>
-                  <p>Our couriers undergo thorough training to ensure packages are handled with care, efficiency, and professionalism.</p>
+                  <p>Our couriers undergo thorough training to ensure packages are handled with care, efficiency, and
+                    professionalism.</p>
                 </div>
               </li>
               <li>
                 <i class="bi bi-hourglass-split"></i>
                 <div>
-                   <h5>Dedicated to Timely Service</h5>
-                  <p>Every team member is committed to delivering parcels on schedule, no matter the distance or weather conditions.</p>
+                  <h5>Dedicated to Timely Service</h5>
+                  <p>Every team member is committed to delivering parcels on schedule, no matter the distance or weather
+                    conditions.</p>
                 </div>
               </li>
               <li>
                 <i class="bi bi-person-raised-hand"></i>
                 <div>
                   <h5>Friendly & Customer-Focused</h5>
-                  <p>We believe in building trust through clear communication, helpful service, and a positive attitude at every doorstep.</p>
+                  <p>We believe in building trust through clear communication, helpful service, and a positive attitude
+                    at every doorstep.</p>
                 </div>
               </li>
             </ul>
@@ -70,14 +77,15 @@ With worldwide reach, we connect people and businesses, one delivery at a time.
     </section>
     <!-- /About Section -->
 
-     <!-- Team Section -->
+    <!-- Team Section -->
     <section id="team" class="team section">
 
       <!-- Section Title -->
       <div class="container section-title" data-aos="fade-up">
         <span>Our Team<br></span>
         <h2>Our Team</h2>
-        <p>Our team is a dedicated group of professionals committed to delivering every parcel with speed, care, and a smile.</p>
+        <p>Our team is a dedicated group of professionals committed to delivering every parcel with speed, care, and a
+          smile.</p>
       </div><!-- End Section Title -->
 
       <div class="container">
@@ -91,7 +99,8 @@ With worldwide reach, we connect people and businesses, one delivery at a time.
                 <h4>Walter White</h4>
                 <span>CEO</span>
                 <p>
-                 Our CEO is a visionary leader dedicated to driving innovation and inspiring excellence across every level of our company
+                  Our CEO is a visionary leader dedicated to driving innovation and inspiring excellence across every
+                  level of our company
                 </p>
                 <div class="social">
                   <a href=""><i class="bi bi-twitter-x"></i></a>
@@ -110,7 +119,8 @@ With worldwide reach, we connect people and businesses, one delivery at a time.
                 <h4>Sarah Jhinson</h4>
                 <span>Team Leader</span>
                 <p>
-                  Our team leader is a motivating force who guides with clarity, fosters collaboration, and ensures every project reaches its full potential.
+                  Our team leader is a motivating force who guides with clarity, fosters collaboration, and ensures
+                  every project reaches its full potential.
                 </p>
                 <div class="social">
                   <a href=""><i class="bi bi-twitter-x"></i></a>
@@ -129,7 +139,8 @@ With worldwide reach, we connect people and businesses, one delivery at a time.
                 <h4>William Anderson</h4>
                 <span>HR Head</span>
                 <p>
-                  Our Head of HR is a compassionate strategist who cultivates a positive workplace culture and empowers our team to thrive.
+                  Our Head of HR is a compassionate strategist who cultivates a positive workplace culture and empowers
+                  our team to thrive.
                 </p>
                 <div class="social">
                   <a href=""><i class="bi bi-twitter-x"></i></a>
@@ -150,22 +161,7 @@ With worldwide reach, we connect people and businesses, one delivery at a time.
   </main>
   <?php require_once('./includes/footer.php') ?>
 
-  <!-- Scroll Top -->
-  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Preloader -->
-  <div id="preloader"></div>
-
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-
-  <!-- Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <?php require_once('./includes/scripts.php') ?>
 
 </body>
 
